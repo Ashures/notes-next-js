74b13d438ba34bdc65fe1f5114a26f10b11cbfe6
Ashures <Ashures>
2023-09-06 00:16:12 +1200

    Removed testing features and tidied up code

--- src/app/Whiteboard.jsx
+++ src/app/Whiteboard.jsx
@@ -23,8 +23,8 @@ export default function Whiteboard() {
                 ...currentNotes,
                 {
                     id: crypto.randomUUID(),
-                    title: 'test title',
-                    text: 'test text'
+                    title: '',
+                    text: ''
                 }
             ];
         })
